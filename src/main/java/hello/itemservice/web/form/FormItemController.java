56ac452b1c2e9d78c7f611d9b6cd004394bb9e18
sk47k <sk47kt@naver.cokm>
2023-10-11 22:43:48 +0900

    최종완성 - 배송방법 선택

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -36,6 +36,15 @@ public class FormItemController {
         return ItemType.values();
     }
 
+    @ModelAttribute("deleveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
+    }
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
